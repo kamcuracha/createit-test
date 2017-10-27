bd09eab70d029aab65388e395dc107428c8bc98c
Paul James Curacha <kamcuracha@yahoo.com>
2017-10-27 08:36:50 +0800

    Add back2top button and script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#return-to-top').fadeIn();
+    } else {
+        $('#return-to-top').fadeOut();
+    }
+});
 
+$(document).ready(function() {
+    $("#return-to-top").click(function(event) {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+});
